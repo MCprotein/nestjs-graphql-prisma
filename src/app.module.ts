a65c40405e028f44b64179e43bd16322b8db5614
chss3339 <chss3339@gmail.com>
2022-08-16 01:47:48 +0900

    feat: prisma로 userService 작성

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
+import { PrismaService } from './prisma/prisma.service';
+import { UsersService } from './users/users.service';
 
 @Module({
   imports: [PostsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService, UsersService],
 })
 export class AppModule {}
---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,18 @@
+import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  onModuleInit() {
+    throw new Error('Method not implemented.');
+  }
+  async OnModuleInit() {
+    await this.$connect();
+  }
+
+  enableShutdownHooks(app: INestApplication) {
+    this.$on('beforeExit', async () => {
+      await app.close();
+    });
+  }
+}
